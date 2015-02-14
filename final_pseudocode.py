3dc496a6ea95f0756a78c1a2d0f26db5a5ed41ed
Paola Garcia <garciaamezola@gmail.com>
2015-02-14 16:16:59 +0000

    The pseudocode for my final in which i will try to find if oxytocin is found in the DNA of animals which do not form social bonds

---
+++ final_pseudocode.py
@@ -0,0 +1,8 @@
+#I will use the DNA sequences of four species and the dna code for the oxytocin hormone to try to find the specific oxytocin coding in the animals DNA
+#for some sequence if DNA not found but RNA found must convert RNA to DNA
+# make sure the sequences are  uppercase
+#calculate the length of the oxytocin sequence
+#calculate the length of the dna sequences 
+#find if the specific DNA sequence for oxytocin is found in the species dna sequences 
+#if found, determine the specific bases where the sequence is found
+ 
